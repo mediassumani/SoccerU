649a9445dd11cb26af0bbd1ee3d5af6be0b09d32
Medi Assumani <mediassumani49@gmail.com>
2019-02-15 17:19:37 -0800

    [Add] league model

--- app/models/league.js
+++ app/models/league.js
@@ -1,2 +1,14 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
+
+
+const LeagueSchema = new Schema({
+
+  name: {type: String, required: true},
+  numberOfTeams: {type: Number},
+  location: {type: String},
+  // TODO: Add a team array property
+  // TODO: Add more fields
+})
+
+module.exports = mongoose.model("League", LeagueSchema)
