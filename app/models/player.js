bc44b28f2e0e6f35867ced053c17f606d01c556c
Medi Assumani <mediassumani49@gmail.com>
2019-02-15 17:26:03 -0800

    [Add] player model

---
+++ app/models/player.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+
+const PlayerSchema = new Schema({
+
+  name: {type: String},
+  age: {type: Int},
+  nationality: {type: String}
+
+  // TODO: Add more fields
+})
+
+module.exports = mongoose.model("Player", PlayerSchema)
