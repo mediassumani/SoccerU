bbfc397ac5a4b4e849b5c619bf7bb9a2c06fd894
Medi Assumani <mediassumani49@gmail.com>
2019-03-02 15:44:51 -0800

    [Update] test coverage for comments

--- test/test-comments.js
+++ test/test-comments.js
@@ -6,33 +6,36 @@ const assert = chai.assert
 const Comment = require('../app/models/comment');
 const Team = require("../app/models/team")
 const League = require("../app/models/league")
-let league
-let team
-let leagueID
-let teamID
 
 chai.use(chaiHttp);
 
 // SAMPLE data to test EDIT, UPDATE, DELETE ROUTES
-const sampleComment = {
+const dummyComment = {
   "title": "The teammates",
   "body": "the teamstes are cherish kim she sucks because she mean as hell"
-};
+}
+
+// SET UP
 const dummyTeam = {
-    players: [],
-    name: "Barcelona",
-    stadium: "Camp Nou",
-    location: "Spain",
-    league: "La Liga Santander",
-    coach: "Some Guy"
-  }
-  
-  const dummyLeague = {
-    teams: [],
-    name: "EPL",
-    numberOfTeams: 20,
-    location: "England, UK"
-  }
+  players: [],
+  name: "Barcelona",
+  stadium: "Camp Nou",
+  location: "Spain",
+  coach: "Some Guy"
+}
+
+const dummyLeague = {
+  teams: [],
+  name: "EPL",
+  numberOfTeams: 20,
+  location: "England, UK"
+}
+
+const dummyPlayer = {
+  name: "Medi",
+  age: 19,
+  nationality: "Congolese"
+}
   
 
 describe('Comments', () => {
@@ -48,53 +51,82 @@ describe('Comments', () => {
         Comment.deleteMany({title: 'The teammates'}).exec( (err, comments) => {
         //comments.remove()
         })
+
+        League.deleteMany({ name: "EPL"}).exec( (error, leagues) => {
+          //leagues.remove()
+        })
+        Team.deleteMany({ name: "Barcelona"}).exec( (error, teams) => {
+          //teams.remove()
+        })
+    
+        Player.deleteMany({ name: "Medi" }).exec( (error, players) => {
+          // players.remove()
+        })
     });
 
-  // TEST ROUTE : INDEX
-//   it('should index ALL comments on /api/v1/leagues/:leagueID/teams/:teamdID/comments GET', (done) => {
-//     chai.request(server)
-//       .get('/api/v1/leagues/:leagueID/teams/:teamID/comments')
-//       .end( (err, res) => {
-//         res.should.have.status(200)
-//         done();
-//       });
-//   });
+  // TEST ROUTE : INDEX ALL COMMENTS
+  it('should index ALL comments on /api/v1/leagues/:leagueID/teams/:teamdID/comments GET', (done) => {
+    
+    const league = new League(dummyLeague)
+    league.save( (err, savedLeague) => {
+      const team = new Team(dummyTeam)
+      team.save( (err, savedTeam) => {
 
-    // // TEST ROUTE : NEW
-    // it('should display new form on /reviews/new GET', (done) => {
-    //   chai.request(server)
-    //     .get(`/reviews/new`)
-    //     .end( (err, res) => {
-    //       res.should.have.status(200);
-    //       res.should.be.html;
-    //       done();
-    //     })
-    // });
+        chai.request(server)
+        .get(`/api/v1/leagues/${savedLeague._id}/teams/${savedTeam._id}/comments`)
+        .end( (err, res) => {
+  
+          // Test Assertions and Assumptions
+          res.should.have.status(200)
+          res.body.should.be.a('array')
+          done()
+        })
+      })
+    })
+  })
+
 
     // // TEST ROUTE : CREATE
-    // it('should return a new created review post on /reviews POST', (done) => {
-    //   chai.request(server)
-    //     .post(`/reviews`)
-    //     .end( (err, res) => {
-    //       res.should.have.status(200);
-    //       res.should.be.html;
-    //       done();
-    //     });
-    // });
+    it('should return a new created review post on /api/v1/leagues/:leagueID/teams/:teamID/comments/new POST', (done) => {
+
+      const league = new League(dummyLeague)
+      league.save( (err, savedLeague) => {
+        const team = new Team(dummyTeam)
+        team.save( (err, savedTeam) => {
+  
+          chai.request(server)
+            .post(`/api/v1/leagues/${savedLeague._id}/teams/${savedTeam._id}/comments/new`)
+            .end( (err, res) => {
+              res.should.have.status(200)
+              done()
+            })
+        })
+      })
+    })
 
     // // TEST ROUTE : SHOW
-    // it('should display  created /reviews/:id GET', (done) => {
-    //   let review = new Review(sampleReview);
-    //   review.save( (err, data) => {
-    //     chai.request(server)
-    //       .get(`/reviews/${data._id}`)
-    //       .end( (err, res) => {
-    //         res.should.have.status(200);
-    //         res.should.be.html;
-    //         done();
-    //       });
-    //   });
-    // });
+    it('should display  created /reviews/:id GET', (done) => {
+      
+      const league = new League(dummyLeague)
+      league.save( (err, savedLeague) => {
+        const team = new Team(dummyTeam)
+        team.save( (err, savedTeam) => {
+          const comment = new Comment(dummyComment)
+          comment.save( (err, savedComment) => {
+            chai.request(server)
+            .get(`/api/v1/leagues/${savedLeague._id}/teams/${savedTeam._id}/comments/${savedComment._id}`)
+            .end( (err, res) => {
+              res.should.have.status(200)
+              res.body.should.have.property('title')
+              res.body.should.have.property('body')
+              assert.typeOf(res.body.title, 'string')
+              assert.typeOf(res.body.body, 'string')
+              done()
+            })
+          })
+        })
+      })
+    })
 
     // // TEST ROUTE : EDIT
     // it('should give the user the ability to edit a review /reviews/:id GET', (done) => {
